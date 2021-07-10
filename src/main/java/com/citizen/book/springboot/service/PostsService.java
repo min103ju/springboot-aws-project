bec9df7bd185535a0f77cc8808f893519242592f
Min Heo <min103ju@gmail.com>
2021-07-10 19:14:35 +0900

    feat(view): 게시글 등록, 수정, 삭제 기능 및 화면 개발

--- src/main/java/com/citizen/book/springboot/service/PostsService.java
+++ src/main/java/com/citizen/book/springboot/service/PostsService.java
@@ -2,12 +2,16 @@ package com.citizen.book.springboot.service;
 
 import com.citizen.book.springboot.domain.posts.Posts;
 import com.citizen.book.springboot.domain.posts.PostsRepository;
+import com.citizen.book.springboot.web.dto.PostsListResponseDto;
 import com.citizen.book.springboot.web.dto.PostsResponseDto;
 import com.citizen.book.springboot.web.dto.PostsSaveRequestDto;
 import com.citizen.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -40,5 +44,21 @@ public class PostsService {
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
         return new PostsResponseDto(posts);
+    }
+
+    // Transaction을 유지하되, 조회 기능만 남겨두기 때문에 조회속도가 개선된다.
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        postsRepository.delete(posts);
     }
 }
--- src/main/java/com/citizen/book/springboot/web/PostsApiController.java
+++ src/main/java/com/citizen/book/springboot/web/PostsApiController.java
@@ -27,4 +27,10 @@ public class PostsApiController {
         System.out.println("PostsApiController.findById");
         return postsService.findById(id);
     }
+
+    @DeleteMapping("/api/v1/posts/{id}")
+    public Long delete(@PathVariable Long id) {
+        postsService.delete(id);
+        return id;
+    }
 }
