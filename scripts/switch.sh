00cbc531dcf2fa03b02d0e3e14f389950857fd07
Min Heo <min103ju@gmail.com>
2021-07-14 09:07:08 +0900

    chore(script): 무중단 스크립트 추가 & 버전 자동 업데이트 수정

---
+++ scripts/switch.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+ABSPATH=$(readlink -find $0)
+ABSDIR=$(dirname $ABSPATH)
+source ${ABSDIR}/profile.sh
+
+funcion switch_proxy()
+{
+  IDLE_PORT=$(find_idle_port)
+
+  echo "> 전환할 Port: $IDLE_PORT"
+  echo "> Port 전환"
+  echo "set \$service_url http://127.0.0.1:${IDLE_PORT};" | sudo tee /etc/nginx/conf.d/service-url.inc
+
+  echo "> nginx reload"
+  sudo systemctl reload nginx
+
+}
